ab74ab3de5406759a53b3547091c8493da662782
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-11-15 18:24:17 -0300

    Added a service for JsonFileProducts
    - Source: https://gist.github.com/bradygaster/3d1fcf43d1d1e73ea5d6c1b5aab40130#file-jsonfileproductservice-cs

---
+++ ContosoCrafts/ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+using ContosoCrafts.Website.Models;
+
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.Website.Services;
+
+public class JsonFileProductService
+{
+    public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+    {
+        WebHostEnvironment = webHostEnvironment;
+    }
+
+    public IWebHostEnvironment WebHostEnvironment { get; }
+
+    private string jsonFileName
+    {
+        get
+        {
+            return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+        }
+    }
+
+    public IEnumerable<Product> GetProducts()
+    {
+        using (var jsonFileReader = File.OpenText(jsonFileName))
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+    }
+}
