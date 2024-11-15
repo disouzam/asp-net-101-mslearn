f313cdf984a3b7c83e6cc8ec5b616ccbd419eb5c
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-11-15 20:06:50 -0300

    Configuring model for index page to call ProductService to get a list of products

--- ContosoCrafts/ContosoCrafts.Website/Pages/Index.cshtml.cs
+++ ContosoCrafts/ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.Website.Services;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.Website.Pages;
@@ -7,13 +12,20 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public JsonFileProductService productService;
+
+    public IEnumerable<Product> Products { get; private set; }
+
+    public IndexModel(
+        ILogger<IndexModel> logger,
+        JsonFileProductService productService)
     {
         this.logger = logger;
+        this.productService = productService;
     }
 
     public void OnGet()
     {
-
+        Products = productService.GetProducts();
     }
 }
